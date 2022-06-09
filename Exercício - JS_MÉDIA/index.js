214a4cbe3bd5d262eab906e67699f104af44882e
Rafael Henrique Soares De Freitas <07568855155@edu.senac.br>
2022-06-09 10:13:04 -0300

    First commit

---
+++ Exercício - JS_MÉDIA/index.js
@@ -0,0 +1,39 @@
+function calcularMedia(){
+
+    let notas = 2;
+
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
+
+    media = (nota1 + nota2)/notas;
+
+    document.getElementById('confirmacao').innerHTML = ('Operação concluída com sucesso!');
+    document.getElementById('info-media').innerHTML = (media + ' PONTOS');
+
+    sistemaMedia(media)
+}
+
+function sistemaMedia(media){
+
+    if (media >= 6){
+        situacao = 'APROVADO(A)';
+        document.getElementById('info-situacao').style.color = 'blue';
+        document.getElementById('info-situacao').style.fontWeight = 'bold';
+    }
+    else if(media < 6 && media >= 0){
+        situacao = 'REPROVADO(A)';
+        document.getElementById('info-situacao').style.color = 'red';
+        document.getElementById('info-situacao').style.fontWeight = 'bold';
+    }
+    else{
+        situacao = 'Situação não calculada';
+    }
+
+    document.getElementById('info-situacao').innerHTML = (situacao);
+}
+
+
+
+
+
+
